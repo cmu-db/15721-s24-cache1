bb0a25a23599810c8df5dc0cbe5ba785821c9f88
xx01cyx <caoyuanxin0531@outlook.com>
2024-02-13 00:42:18 +0000

    fix typo

--- storage-client/src/lib.rs
+++ storage-client/src/lib.rs
@@ -4,34 +4,34 @@ use arrow::record_batch::RecordBatch;
 use datafusion::{error::DataFusionError, physical_plan::SendableRecordBatchStream};
 
 /// Result type that the storage node would return to the execution engine. We use
-/// `DataFusionError` here to align with the execution engin.
+/// `DataFusionError` here to align with the execution engine.
 pub type StorageResult<T> = anyhow::Result<T, DataFusionError>;
 
-/// Types for table id and column id. Need to be consistent among all components
+/// Id types for table, column, and record. Need to be consistent among all components
 /// (e.g. execution engine). We don't want to make any type generic here just for the id,
 /// so we simply define them here. Might refine later.
 pub type TableId = u64;
 pub type ColumnId = u64;
 pub type RecordId = u64;
 
-/// [`StorageRequestType`] specifies the types of the requests that the execution engine
-/// might issue to the storage node.
+/// [`StorageRequest`] specifies the requests that the execution engine might issue to
+/// the storage node.
 ///
 /// Currently we assume the execution engine only requests the whole table/column. We may
 /// add `std::ops::RangeBounds` later to support range query from the execution engine.
 pub enum StorageRequest {
-    /// Request a whole table from the underlying storage.
+    /// Requests a whole table from the underlying storage.
     Table(TableId),
-    /// Request one or more columns from the underlying storage.
+    /// Requests one or more columns from the underlying storage.
     Columns(TableId, Vec<ColumnId>),
-    /// Request one or more tuples from the underlying storage.
+    /// Requests one or more tuples from the underlying storage.
     /// FIXME: Do we really need this?
     Tuple(Vec<RecordId>),
 }
 
 /// [`StorageClient`] provides the interface for the execution engine to query data from the
-/// storage node. It resolves the physical location of the tables/columns by querying the
-/// catalog node, and then sends the request to the storage node to get the data from the
+/// storage node. It resolves the physical location of the tables/columns/tuples by querying
+/// the catalog node, and then sends the request to the storage node to get the data from the
 /// underlying storage.
 #[async_trait::async_trait]
 pub trait StorageClient: Send + Sync + 'static {
