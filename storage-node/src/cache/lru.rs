8bb54f3927b949bcff19da658d54ab5895985917
xx01cyx <caoyuanxin0531@outlook.com>
2024-02-18 01:08:16 +0000

    make lru cache generic

--- storage-node/src/cache/lru.rs
+++ storage-node/src/cache/lru.rs
@@ -1,29 +1,33 @@
+use std::collections::HashMap;
+
 use datafusion::execution::DiskManager;
 
 use super::{ParpulseCache, ParpulseCacheKey, ParpulseCacheValue};
 
 // Just an example. Feel free to modify.
-pub struct LruCache {}
+pub struct LruCache<K, V> {
+    kv: HashMap<K, V>,
+}
 
-impl LruCache {
+impl<K, V> LruCache<K, V> {
     pub fn new() -> Self {
-        Self {}
+        Self { kv: HashMap::new() }
     }
 
-    pub fn get(&mut self, key: &ParpulseCacheKey) -> Option<ParpulseCacheValue> {
+    pub fn get(&mut self, key: &K) -> Option<V> {
         todo!()
     }
 
-    pub fn put(&mut self, key: ParpulseCacheKey, value: ParpulseCacheValue) -> bool {
+    pub fn put(&mut self, key: K, value: V) -> bool {
         todo!()
     }
 
-    pub fn pop(&mut self) -> Option<ParpulseCacheValue> {
+    pub fn pop(&mut self) -> Option<V> {
         todo!()
     }
 }
 
-impl ParpulseCache for LruCache {
+impl ParpulseCache for LruCache<ParpulseCacheKey, ParpulseCacheValue> {
     fn get_value(&mut self, key: &ParpulseCacheKey) -> Option<ParpulseCacheValue> {
         self.get(key)
     }
