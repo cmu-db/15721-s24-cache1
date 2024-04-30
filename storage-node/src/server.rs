c9cc2c1f86b8d786a5967ba69672667caec4782d
Lan Lou <lanlou1554@gmail.com>
2024-04-30 17:00:16 -0400

    chore: modify configuration

--- storage-node/src/server.rs
+++ storage-node/src/server.rs
@@ -12,14 +12,14 @@ use crate::{
 };
 
 const CACHE_BASE_PATH: &str = "cache/";
-const DATA_STORE_CACHE_NUMBER: usize = 6;
+const DATA_STORE_CACHE_NUMBER: usize = 3;
 
 pub async fn storage_node_serve(ip_addr: &str, port: u16) -> ParpulseResult<()> {
     // Should at least be able to store one 100MB file in the cache.
     // TODO: Read the type of the cache from config.
-    let dummy_size_per_disk_cache = 100 * 1024 * 1024;
-    let dummy_size_per_mem_cache = 100 * 1024;
-    let dummy_mem_max_file_cache = 10 * 1024;
+    let dummy_size_per_disk_cache = 1000 * 1024 * 1024;
+    let dummy_size_per_mem_cache = 200 * 1024 * 1024;
+    let dummy_mem_max_file_cache = 10 * 1024 * 1024 + 1;
 
     let mut data_store_caches = Vec::new();
 
