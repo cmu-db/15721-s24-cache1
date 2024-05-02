b292f8f1bab651be6f643aa9bc82d4f0638d6780
xx01cyx <caoyuanxin0531@outlook.com>
2024-05-02 03:59:07 +0000

    add client handshake time & bump version to 0.1.7

--- storage-client/src/client.rs
+++ storage-client/src/client.rs
@@ -236,7 +236,13 @@ impl StorageClient for StorageClientImpl {
         };
 
         // Then we need to send the request to the storage server.
+        let handshake_start = Instant::now();
         let client = Client::new();
+        let handshake_time = handshake_start.elapsed();
+        info!(
+            "[Parpulse Timer] client handshake time for request {}: {:?}",
+            request_id, handshake_time
+        );
         let (url, params) = self.get_request_url_and_params(location, request_id)?;
         let url = Url::parse_with_params(&url, params)?;
         let get_response_start = Instant::now();
